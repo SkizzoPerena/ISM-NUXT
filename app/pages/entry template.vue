c2bab085fa341252039de5428249540971ef44c8
SkizzoPerena <skizzoperena@gmail.com>
2026-02-06 01:33:14 +0800

    entry template init

--- app/pages/entry template.vue
+++ app/pages/entry template.vue
@@ -11,9 +11,12 @@ const API_BASE = 'https://noteworthy-z9k0.onrender.com'
 <template>
 <UContainer>
     <UPageCard>
-        <div class="text-lg font-bold">Journal Name: Skizzo's Submission</div>
+        <!-- "Journal Name" and "Skizzo's" should change depending on journal name and student's first name -->
+        <div class="text-lg font-bold">Journal Name: Skizzo's Submission</div> 
         <USeparator />
         <UPageGrid>
+
+            <!-- template for questions -->
             <UPageCard title="Question 1:" description="blah blah blah">
                 <USeparator/><div class="flex align-left">
                 <div class="font-semibold mr-1">Answer:</div>
