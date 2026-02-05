e3efc5dbc9daad620961319b5e1dcd90ca4a5a0e
SkizzoPerena <skizzoperena@gmail.com>
2026-02-06 00:28:02 +0800

    created entry template for other entries (assessments, etc)

---
+++ app/pages/entry template.vue
@@ -0,0 +1,26 @@
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'dashboard',
+})
+
+const API_BASE = 'https://noteworthy-z9k0.onrender.com'
+
+</script>
+
+<template>
+<UContainer>
+    <UPageCard>
+        <div class="text-lg font-bold">Journal Name: Skizzo's Submission</div>
+        <USeparator />
+        <UPageGrid>
+            <UPageCard title="Question 1:" description="blah blah blah">
+                <USeparator/><div class="flex align-left">
+                <div class="font-semibold mr-1">Answer:</div>
+                <div class="">test</div></div>
+            </UPageCard>
+            
+        </UPageGrid>
+    </UPageCard>
+</UContainer>
+</template>
